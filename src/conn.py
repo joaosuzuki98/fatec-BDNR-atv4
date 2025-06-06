c1269d7cd849cd50a7fa4f091fe3a6ed3a0cf0bc
João Suzuki <joaosuzuki98@outlook.com>
2025-06-05 23:18:45 -0300

    Change conn to a class element

--- src/conn.py
+++ src/conn.py
@@ -6,9 +6,14 @@ import os
 load_dotenv()
 token = os.getenv("TOKEN")
 
-client = DataAPIClient(token)
-db = client.get_database_by_api_endpoint(
-      "https://d9ef376d-b4eb-4def-a292-c2e261be2421-us-east1.apps.astra.datastax.com"
-)
 
-print(f"Connected to Astra DB: {db.list_collection_names()}")
+class Connection:
+    def __init__(self):
+        try:
+            self.client = DataAPIClient(token)
+            self.db = self.client.get_database_by_api_endpoint(
+                    "https://d9ef376d-b4eb-4def-a292-c2e261be2421-us-east1."
+                    "apps.astra.datastax.com"
+            )
+        except:
+            print("Um erro aconteceu ao tentar conectar ao astradb")
