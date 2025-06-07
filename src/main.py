1db2a7878ee3be606d7823d6a1a1a00e24d01a64
João Suzuki <joaosuzuki98@outlook.com>
2025-06-07 13:44:11 -0300

    Fix minor errors

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
-from models import user
+from models import user, seller
 
 
 us = user.User()
+ve = seller.Seller()
 
 if __name__ == "__main__":
     while True:
@@ -15,81 +16,34 @@ if __name__ == "__main__":
             "[5] Sair\n"
         )
 
-        match choice:
-            case "1":
-                choice = input(
-                    "O que deseja fazer?\n"
-                    "[1] Inserir\n"
-                    "[2] Atualizar\n"
-                    "[3] Voltar\n"
-                )
+        while True:
+            match choice:
+                case "1":
+                    choice = input(
+                        "O que deseja fazer?\n"
+                        "[1] Inserir\n"
+                        "[2] Atualizar\n"
+                        "[3] Voltar\n"
+                    )
 
-                match choice:
-                    case "1":
-                        name = input("Digite o nome do usuário: ")
-                        email = input("Digite o email do usuário: ")
-                        cpf = input("Digite o CPF do usuário: ")
-                        password = input("Digite a senha do usuário: ")
+                    match choice:
+                        case "1":
+                            name = input("Digite o nome do usuário: ")
+                            email = input("Digite o email do usuário: ")
+                            cpf = input("Digite o CPF do usuário: ")
+                            password = input("Digite a senha do usuário: ")
 
-                        addresses = []
+                            addresses = []
 
-                        add_address = input(
-                            "Deseja adicionar endereços? s/n: "
-                        )
-                        while add_address.lower() == "s":
-                            street = input("Digite a rua: ")
-                            neighborhood = input("Digite o bairro: ")
-                            city = input("Digite a cidade: ")
-                            state = input("Digite o estado: ")
-                            cep = input("Digite o cep: ")
-                            number = input("Digite o número: ")
-
-                            addresses.append({
-                                "rua": street,
-                                "bairro": neighborhood,
-                                "cidade": city,
-                                "estado": state,
-                                "cep": cep,
-                                "numero": number
-                            })
-
-                            stop = input(
-                                "Deseja adicionar mais um endereço? s/n: "
+                            add_address = input(
+                                "Deseja adicionar endereços? s/n: "
                             )
-
-                            if stop.lower() != "s":
-                                break
-
-                        us.insert(
-                            name,
-                            email,
-                            cpf,
-                            addresses,
-                            password
-                        )
-                    case "2":
-                        user_id = input(
-                            "Digite o ID do usuário que deseja atualizar: "
-                        )
-
-                        print("Deixe o campo vazio se não quiser atualizar.")
-
-                        name = input("Novo nome: ") or None
-                        email = input("Novo email: ") or None
-                        cpf = input("Novo CPF: ") or None
-                        password = input("Nova senha: ") or None
-
-                        addresses = []
-                        change_address = input(
-                            "Deseja atualizar os endereços? (s/n): "
-                        )
-                        if change_address.lower() == "s":
-                            while True:
+                            while add_address.lower() == "s":
                                 street = input("Digite a rua: ")
                                 neighborhood = input("Digite o bairro: ")
                                 city = input("Digite a cidade: ")
                                 state = input("Digite o estado: ")
-                                cep = input("Digite o CEP: ")
+                                cep = input("Digite o cep: ")
                                 number = input("Digite o número: ")
 
                                 addresses.append({
@@ -102,87 +56,137 @@ if __name__ == "__main__":
                                 })
 
                                 stop = input(
-                                    "Deseja adicionar mais um endereço? "
-                                    "(s/n): "
+                                    "Deseja adicionar mais um endereço? s/n: "
                                 )
+
                                 if stop.lower() != "s":
                                     break
-                        else:
-                            addresses = None
 
-                        us.update(
-                            user_id,
-                            name,
-                            email,
-                            cpf,
-                            addresses,
-                            password
-                        )
-                    case "3":
-                        break
-                    case _:
-                        print("Selecione somente as opções listadas.")
-            case "2":
-                choice = input(
-                    "O que deseja fazer?\n"
-                    "[1] Inserir\n"
-                    "[2] Voltar\n"
-                )
-
-                match choice:
-                    case "1":
-                        name = input("Digite o nome do vendedor: ")
-                        email = input("Digite o email do vendedor: ")
-                        cpf = input("Digite o CPF do vendedor: ")
-                        cnpj = input("Digite o CNPJ do vendedor: ")
-                        password = input("Digite a senha do vendedor: ")
-
-                        addresses = []
-
-                        add_address = input(
-                            "Deseja adicionar endereços? s/n: "
-                        )
-
-                        while add_address.lower() == "s":
-                            street = input("Digite a rua: ")
-                            neighborhood = input("Digite o bairro: ")
-                            city = input("Digite a cidade: ")
-                            state = input("Digite o estado: ")
-                            cep = input("Digite o cep: ")
-                            number = input("Digite o número: ")
-
-                            addresses.append({
-                                "rua": street,
-                                "bairro": neighborhood,
-                                "cidade": city,
-                                "estado": state,
-                                "cep": cep,
-                                "numero": number
-                            })
-
-                            stop = input(
-                                "Deseja adicionar mais um endereço? s/n: "
+                            us.insert(
+                                name,
+                                email,
+                                cpf,
+                                addresses,
+                                password
+                            )
+                        case "2":
+                            user_id = input(
+                                "Digite o ID do usuário que deseja atualizar: "
                             )
 
-                            if stop.lower() != "s":
-                                break
+                            print(
+                                "Deixe o campo vazio se não quiser atualizar."
+                            )
 
-                        us.insert(
-                            name,
-                            email,
-                            cpf,
-                            addresses,
-                            password
-                        )
-                    case "2":
-                        break
-                    case _:
-                        print("Selecione somente as opções listadas.")
-            case "3":
-                pass
-            case "4":
-                pass
-            case "5":
-                break
-            case _:
-                print("Por favor, selecione apenas as opções listadas.")
+                            name = input("Novo nome: ") or None
+                            email = input("Novo email: ") or None
+                            cpf = input("Novo CPF: ") or None
+                            password = input("Nova senha: ") or None
+
+                            addresses = []
+                            change_address = input(
+                                "Deseja atualizar os endereços? (s/n): "
+                            )
+                            if change_address.lower() == "s":
+                                while True:
+                                    street = input("Digite a rua: ")
+                                    neighborhood = input("Digite o bairro: ")
+                                    city = input("Digite a cidade: ")
+                                    state = input("Digite o estado: ")
+                                    cep = input("Digite o CEP: ")
+                                    number = input("Digite o número: ")
+
+                                    addresses.append({
+                                        "rua": street,
+                                        "bairro": neighborhood,
+                                        "cidade": city,
+                                        "estado": state,
+                                        "cep": cep,
+                                        "numero": number
+                                    })
+
+                                    stop = input(
+                                        "Deseja adicionar mais um endereço? "
+                                        "(s/n): "
+                                    )
+                                    if stop.lower() != "s":
+                                        break
+                            else:
+                                addresses = None
+
+                            us.update(
+                                user_id,
+                                name,
+                                email,
+                                cpf,
+                                addresses,
+                                password
+                            )
+                        case "3":
+                            break
+                        case _:
+                            print("Selecione somente as opções listadas.")
+                case "2":
+                    choice = input(
+                        "O que deseja fazer?\n"
+                        "[1] Inserir\n"
+                        "[2] Voltar\n"
+                    )
+
+                    match choice:
+                        case "1":
+                            name = input("Digite o nome do vendedor: ")
+                            email = input("Digite o email do vendedor: ")
+                            cpf = input("Digite o CPF do vendedor: ")
+                            cnpj = input("Digite o CNPJ do vendedor: ")
+                            password = input("Digite a senha do vendedor: ")
+
+                            addresses = []
+
+                            add_address = input(
+                                "Deseja adicionar endereços? s/n: "
+                            )
+
+                            while add_address.lower() == "s":
+                                street = input("Digite a rua: ")
+                                neighborhood = input("Digite o bairro: ")
+                                city = input("Digite a cidade: ")
+                                state = input("Digite o estado: ")
+                                cep = input("Digite o cep: ")
+                                number = input("Digite o número: ")
+
+                                addresses.append({
+                                    "rua": street,
+                                    "bairro": neighborhood,
+                                    "cidade": city,
+                                    "estado": state,
+                                    "cep": cep,
+                                    "numero": number
+                                })
+
+                                stop = input(
+                                    "Deseja adicionar mais um endereço? s/n: "
+                                )
+
+                                if stop.lower() != "s":
+                                    break
+
+                            ve.insert(
+                                name,
+                                email,
+                                cpf,
+                                addresses,
+                                password
+                            )
+                        case "2":
+                            break
+                        case _:
+                            print("Selecione somente as opções listadas.")
+                case "3":
+                    pass
+                case "4":
+                    pass
+                case "5":
+                    break
+                case _:
+                    print("Por favor, selecione apenas as opções listadas.")
